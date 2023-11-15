a480a4428ae6335eace2bcb48f97c6f690dab5c4
Dev sam <nexigen0@gmail.com>
2023-11-15 07:09:45 +0100

    working on src files

--- src/synthetix/utils/node_modules/eth-utils/@types/idna-uts46-hx/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "idna-uts46-hx" {
-  export interface IToAsciiOptions {
-    useStd3ASCII?: boolean;
-    transitional?: boolean;
-  }
-
-  export const toAscii: (value: string, options?: IToAsciiOptions) => string;
-}
--- src/synthetix/utils/node_modules/eth-utils/@types/keccak/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "keccak" {
-  import { Hash } from "crypto";
-
-  type Types = "keccak256" | "keccak512";
-  const keccak: (type: Types) => Hash;
-
-  export = keccak;
-}
